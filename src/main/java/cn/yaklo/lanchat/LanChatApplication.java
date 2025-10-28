513f473e200c955d7c6e7c69d7e93bdba7c1953c
Yaklo <yaklo@skyesc.com>
2025-10-28 14:48:00 +0800

    初始化项目结构，添加基本的Spring Boot配置和示例控制器

---
+++ src/main/java/cn/yaklo/lanchat/LanChatApplication.java
@@ -0,0 +1,13 @@
+package cn.yaklo.lanchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/yaklo/lanchat/LanChatApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yaklo.lanchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
